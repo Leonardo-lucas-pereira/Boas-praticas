2ee1207b5926a625879633a493fe6639d39545fb
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-07-09 15:12:16 -0300

    Padrão Factory e typing union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fila_normal import FilaNormal
-from fila_prioritaria import FilaPrioritaria
+# from fila_normal import FilaNormal
+# from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
 
 
 # fila_teste = filanormal()
@@ -10,9 +11,15 @@ from fila_prioritaria import FilaPrioritaria
 # print(fila_teste.chamacliente(5))
 # print(fila_teste.chamacliente(10))
 
-fila_teste_2 = FilaPrioritaria()
-fila_teste_2.atualizafila()
-fila_teste_2.atualizafila()
-fila_teste_2.atualizafila()
-print(fila_teste_2.chamacliente(10))
-print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+# fila_teste_2 = FilaPrioritaria()
+# fila_teste_2.atualizafila()
+# fila_teste_2.atualizafila()
+# fila_teste_2.atualizafila()
+# print(fila_teste_2.chamacliente(10))
+# print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+
+test_fabrica = FabricaFila.pega_fila('normal')
+test_fabrica.atualiza_fila()
+test_fabrica.atualiza_fila()
+test_fabrica.atualiza_fila()
+print(test_fabrica.chama_cliente(5))
