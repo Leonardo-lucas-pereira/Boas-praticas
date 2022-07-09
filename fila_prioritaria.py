254a994a7b106767044b70cea77a74893d0e3958
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-07-09 15:01:08 -0300

    Criando constantes

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,22 +1,23 @@
 from fila_base import FilaBase
+from costantes import CODIGO_PRIORITARIO
 
 
 class FilaPrioritaria(FilaBase):
     def gera_senha_atual(self) -> None:
-        self.senha_atual = f'PR{self.codigo}'
+        self.senha_atual = f'{CODIGO_PRIORITARIO}{self.codigo}'
 
     def chama_cliente(self, caixa: int) -> None:
         display = []
-		cliente_atual = self.fila.pop(0)
-		display.append(f'Cliente: ]{cliente_atual} - Caixa {caixa}')
+        cliente_atual = self.fila.pop(0)
+        display.append(f'Cliente: ]{cliente_atual} - Caixa {caixa}')
 
-		if len(self.fila) >= 3:
-			display.append(f'Próximo: {self.fila[0]}')
-			display.append(f'Fique atento: {self.fila[1]}')
+        if len(self.fila) >= 3:
+            display.append(f'Próximo: {self.fila[0]}')
+            display.append(f'Fique atento: {self.fila[1]}')
 
-		self.clientes_atendidos.append(cliente_atual)
+        self.clientes_atendidos.append(cliente_atual)
 
-		return display
+        return display
         
     def estatistica(self, dia: str, agencia: str, flag: str) -> dict:
         if flag != 'detail':
